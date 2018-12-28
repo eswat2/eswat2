c39ad5608a930ba9ec0fdf28ff83e3d28e668cdf
Richard Hess <eswat2@gmail.com>
2018-12-27 23:00:20 -0800

    add reducer (curried) and support more data as arrays

--- build.js
+++ build.js
@@ -20,25 +20,30 @@ const data = {
   name: chalk.white.bold('              Richard Hess'),
   handle: chalk.cyan('eswat2'),
   work: chalk.white('Principle UI Engineer, UX/UI Engineering at Skava'),
-  gmail: chalk.cyan('eswat2') + chalk.gray('@gmail.com'),
+  oss: [],
+  gmail: [
+    chalk.cyan('eswat2') + chalk.gray('@gmail.com'),
+    chalk.cyan('eswat42') + chalk.gray('@gmail.com'),
+  ],
   twitter: chalk.gray('https://twitter.com/') + chalk.cyan('eswat2'),
   npm: chalk.gray('https://npmjs.com/~') + chalk.cyan('eswat2'),
   github: chalk.gray('https://github.com/') + chalk.cyan('eswat2'),
   linkedin: chalk.gray('https://linkedin.com/in/') + chalk.cyan('eswat'),
-  demo: [ 
+  web: [],
+  apps: [ 
     chalk.magenta('https://funnel-gfx.herokuapp.com/'),
     chalk.magenta('https://fire-notes.herokuapp.com/'),
     chalk.magenta('https://git-notes.herokuapp.com/'), 
   ],
   npx: chalk.gray('npx') + ' ' + chalk.cyan('eswat2'),
   labelWork: chalk.white.bold('       Work:'),
-  labelOpenSource: chalk.white.bold('Open Source:'),
+  labelOss: chalk.white.bold('Open Source:'),
   labelGmail: chalk.white.bold('      Gmail:'),
   labelTwitter: chalk.white.bold('    Twitter:'),
   labelnpm: chalk.white.bold('        npm:'),
   labelGitHub: chalk.white.bold('     GitHub:'),
   labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelDemo: chalk.white.bold('       demo:'),
+  labelApps: chalk.white.bold('       apps:'),
   labelPad: chalk.white.bold('            '),
   labelWeb: chalk.white.bold('        Web:'),
   labelCard: chalk.white.bold('       Card:')
@@ -48,28 +53,41 @@ const data = {
 const newline = '\n'
 const heading = `${data.name} - ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
-const opensourcing = data.opensource ? `${data.labelOpenSource}  ${data.opensource}` : undefined
-const gmailing = data.gmail ? `${data.labelGmail}  ${data.gmail}` : undefined
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const demoing = data.demo ? data.demo.reduce((prev, current, indx) => { return prev + (indx == 0 ? data.labelDemo : data.labelPad) + '  ' + current + newline }, '') : undefined
-const webing = data.web ? `${data.labelWeb}  ${data.web}` : undefined
 const carding = `${data.labelCard}  ${data.npx}`
+
+const reducer = (label, pad, newline) => {
+  return (prev, current, indx) => {
+    return prev + (indx == 0 ? label : pad) + '  ' + current + newline
+  }
+}
+
+const notEmpty = (array) => {
+  return array && (array.length > 0)
+}
+
+const { apps, gmail, oss, web, labelApps, labelGmail, labelOss, labelPad, labelWeb } = data
+const gmailing = notEmpty(gmail) ? gmail.reduce(reducer(labelGmail, labelPad, newline), '') : undefined
+const apping = notEmpty(apps) ? apps.reduce(reducer(labelApps, labelPad, newline), '') : undefined
+const ossing = notEmpty(oss) ? oss.reduce(reducer(labelOss, labelPad, newline), '') : undefined
+const webing = notEmpty(web) ? web.reduce(reducer(labelWeb, labelPad, newline), '') : undefined
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output = heading + // data.name + data.handle
                newline + newline + // Add one whole blank line
-               working + newline + // data.labelWork + data.work
-               (opensourcing ? opensourcing + newline + newline : '') + // data.labelOpenSource + data.opensource
-               (gmailing ? gmailing + newline : '') + 
-               twittering + newline + // data.labelTwitter + data.twitter
-               npming + newline + // data.labelnpm + data.npm
-               githubing + newline + // data.labelGitHub + data.github
-               linkedining + newline + // data.labelLinkedIn + data.linkedin
-               (demoing ? demoing + newline : '') +
-               (webing ? webing + newline : '' ) + newline + // data.labelWeb + data.web
-               carding // data.labelCard + data.npx
+               working + newline +
+               (ossing ? ossing + newline : '') +
+               (gmailing ? gmailing : '') + 
+               twittering + newline + 
+               npming + newline + 
+               githubing + newline + 
+               linkedining + newline + 
+               (webing ? webing : '' ) + 
+               (apping ? apping : '') +
+               newline +
+               carding
 
 fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
