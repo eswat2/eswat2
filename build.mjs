be37e219f75b53ffc448c593403c30d0cefa291e
Richard Hess <eswat2@gmail.com>
2022-05-02 16:57:51 -0700

    tweak pico

--- build.mjs
+++ build.mjs
@@ -1,10 +1,12 @@
-import pico from "picocolors"
 import boxen from "boxen"
+import pico from "picocolors"
 
 import fs from 'fs'
 import path from 'path'
 
 const __dirname = path.resolve();
+
+const { white, cyan, gray, green, magenta, bold } = pico.createColors()
 
 // Define options for Boxen
 const boxOptions = {
@@ -36,72 +38,72 @@ const socialData = tags.reduce((obj, tag) => {
 }, {})
 
 // Text + pico definitions
-//   oss: pico.white('Node.js Community Committee ') + pico.green('⬢'),
-//   web: pico.cyan('https://bnb.im'),
+//   oss: white('Node.js Community Committee ') + green('⬢'),
+//   web: cyan('https://bnb.im'),
 //
 // NOTE:  you push your specific data into the socialData arrays...
 //
-socialData.work.push(pico.white("Portfolio Technical Architect - UI/UX"))
-socialData.work.push(pico.white("Lead Technical Architect - UI"))
-socialData.work.push(pico.white("Lead Software Engineer - UI"))
-socialData.work.push(pico.white("UX/UI Engineering"))
-socialData.gmail.push(pico.cyan("eswat2") + pico.gray("@gmail.com"))
-socialData.gmail.push(pico.cyan("eswat42") + pico.gray("@gmail.com"))
+socialData.work.push(white("Portfolio Technical Architect - UI/UX"))
+socialData.work.push(white("Lead Technical Architect - UI"))
+socialData.work.push(white("Lead Software Engineer - UI"))
+socialData.work.push(white("UX/UI Engineering"))
+socialData.gmail.push(cyan("eswat2") + gray("@gmail.com"))
+socialData.gmail.push(cyan("eswat42") + gray("@gmail.com"))
 socialData.twitter.push(
-  pico.gray("https://twitter.com/") + pico.cyan("eswat2")
+  gray("https://twitter.com/") + cyan("eswat2")
 )
-socialData.npm.push(pico.gray("https://npmjs.com/~") + pico.cyan("eswat2"))
-socialData.github.push(pico.gray("https://github.com/") + pico.cyan("eswat2"))
+socialData.npm.push(gray("https://npmjs.com/~") + cyan("eswat2"))
+socialData.github.push(gray("https://github.com/") + cyan("eswat2"))
 socialData.github.push(
-  pico.gray("https://") + pico.cyan("eswat2") + pico.gray(".github.io")
+  gray("https://") + cyan("eswat2") + gray(".github.io")
 )
 
-socialData.domains.push(pico.green("eswat2.dev"))
-socialData.domains.push(pico.green("eswat42.dev"))
-socialData.domains.push(pico.green("richardhess.dev"))
+socialData.domains.push(green("eswat2.dev"))
+socialData.domains.push(green("eswat42.dev"))
+socialData.domains.push(green("richardhess.dev"))
 
 socialData.code.push(
-  pico.gray("https://codesandbox.io/u/") + pico.cyan("eswat2")
+  gray("https://codesandbox.io/u/") + cyan("eswat2")
 )
 socialData.linkedin.push(
-  pico.gray("https://www.linkedin.com/in/") + pico.cyan("eswat")
+  gray("https://www.linkedin.com/in/") + cyan("eswat")
 )
-socialData.angel.push(pico.gray("https://angel.co/") + pico.cyan("eswat2"))
-socialData.apps.push(pico.magenta("https://eswat2.github.io/auto-gql"))
-socialData.apps.push(pico.magenta("https://fire-notes.herokuapp.com"))
-socialData.apps.push(pico.magenta("https://funnel-r3t.vercel.app"))
-socialData.apps.push(pico.magenta("https://funnel-s4e.vercel.app"))
-socialData.apps.push(pico.magenta("https://funnel-sld.vercel.app"))
-socialData.apps.push(pico.magenta("https://funnel-vue.vercel.app"))
-socialData.apps.push(pico.magenta("https://git-notes-eswat2.vercel.app"))
-socialData.apps.push(pico.magenta("https://s4e-autos.vercel.app"))
-socialData.apps.push(pico.magenta("https://s4e-ikon-proofs.vercel.app"))
-socialData.apps.push(pico.magenta("https://s4e-proofs.vercel.app"))
-socialData.apps.push(pico.magenta("https://wc-autos.vercel.app"))
-socialData.apps.push(pico.magenta("https://wc-funnel.vercel.app"))
-socialData.apps.push(pico.magenta("https://wc-proofs.vercel.app"))
-socialData.apps.push(pico.magenta("https://wc-sudoku.vercel.app"))
+socialData.angel.push(gray("https://angel.co/") + cyan("eswat2"))
+socialData.apps.push(magenta("https://eswat2.github.io/auto-gql"))
+socialData.apps.push(magenta("https://fire-notes.herokuapp.com"))
+socialData.apps.push(magenta("https://funnel-r3t.vercel.app"))
+socialData.apps.push(magenta("https://funnel-s4e.vercel.app"))
+socialData.apps.push(magenta("https://funnel-sld.vercel.app"))
+socialData.apps.push(magenta("https://funnel-vue.vercel.app"))
+socialData.apps.push(magenta("https://git-notes-eswat2.vercel.app"))
+socialData.apps.push(magenta("https://s4e-autos.vercel.app"))
+socialData.apps.push(magenta("https://s4e-ikon-proofs.vercel.app"))
+socialData.apps.push(magenta("https://s4e-proofs.vercel.app"))
+socialData.apps.push(magenta("https://wc-autos.vercel.app"))
+socialData.apps.push(magenta("https://wc-funnel.vercel.app"))
+socialData.apps.push(magenta("https://wc-proofs.vercel.app"))
+socialData.apps.push(magenta("https://wc-sudoku.vercel.app"))
 
 const data = {
-  name: pico.white(pico.bold("Richard Hess")),
-  handle: pico.cyan("eswat2"),
-  npx: pico.gray("npx") + " " + pico.cyan("eswat2"),
+  name: white(bold("Richard Hess")),
+  handle: cyan("eswat2"),
+  npx: gray("npx") + " " + cyan("eswat2"),
   social: socialData,
   labels: {
-    angel: pico.white(pico.bold("   AngelList:")),
-    apps: pico.white(pico.bold("        apps:")),
-    card: pico.white(pico.bold("        Card:")),
-    code: pico.white(pico.bold("      Coding:")),
-    domains: pico.white(pico.bold("     Domains:")),
-    github: pico.white(pico.bold("      GitHub:")),
-    gmail: pico.white(pico.bold("       Gmail:")),
-    linkedin: pico.white(pico.bold("    LinkedIn:")),
-    npm: pico.white(pico.bold("         npm:")),
-    oss: pico.white(pico.bold(" Open Source:")),
-    pad: pico.white(pico.bold("             ")),
-    twitter: pico.white(pico.bold("     Twitter:")),
-    web: pico.white(pico.bold("         Web:")),
-    work: pico.white(pico.bold("        Work:")),
+    angel: white(bold("   AngelList:")),
+    apps: white(bold("        apps:")),
+    card: white(bold("        Card:")),
+    code: white(bold("      Coding:")),
+    domains: white(bold("     Domains:")),
+    github: white(bold("      GitHub:")),
+    gmail: white(bold("       Gmail:")),
+    linkedin: white(bold("    LinkedIn:")),
+    npm: white(bold("         npm:")),
+    oss: white(bold(" Open Source:")),
+    pad: white(bold("             ")),
+    twitter: white(bold("     Twitter:")),
+    web: white(bold("         Web:")),
+    work: white(bold("        Work:")),
   },
 }
 
@@ -142,5 +144,5 @@ const output =
 
 fs.writeFileSync(
   path.join(__dirname, "bin/output"),
-  pico.green(boxen(output, boxOptions))
+  green(boxen(output, boxOptions))
 )
