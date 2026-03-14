7ad92f784152790dd69ad5836615284fa33c3d76
Richard Hess <eswat2@gmail.com>
2026-03-14 15:24:48 -0700

    testing new bumpp config

--- bump.config.ts
+++ bump.config.ts
@@ -3,15 +3,13 @@ import { defineConfig } from 'bumpp'
 import { red, green } from 'yoctocolors'
 
 export default defineConfig({
-  // enable git operations by default if you like
-  // commit can be true or a template string
-  commit: 'release v%s',   // e.g. "release v0.0.983"
+  commit: true,
   tag: true,
   push: true,
 
   // this runs *after* bumpp has updated the version (but before commit/tag)
   async execute(ctx) {
-    // console.log(ctx)
+    console.log(ctx)
     const op = ctx as any
     const oldVersion = op.state?.currentVersion
     const newVersion = op.state?.newVersion
